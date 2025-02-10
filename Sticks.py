3253fab51ec5b555fb269b3437cb1e7d9f6e3388
yell0wrat <superpikachubro@gmail.com>
2025-02-09 19:51:50 -0500

    Update Sticks.py

--- Sticks.py
+++ Sticks.py
@@ -1,32 +1,48 @@
 import random
-sticks = random.randint(10,50)
-print('there are', sticks,'sticks to remove')
+import time
+sticks = random.randint(10, 1000)
+time.sleep(1)
+print('there are', sticks, 'sticks to remove')
+time.sleep(1)
 
-# turn initialized to start the process of turns
+# added this so you can see what you and the computer do without it blazing past quickly
+
+max_Remove = int(input('pick the max amount of sticks to remove (cannot pick more than a quarter of the sticks): '))
+if max_Remove > sticks/4:
+    print('hey, you can not remove more sticks than the amount of sticks here!')
+    time.sleep(1)
+    max_Remove = int(input('pick the max amount of sticks to remove: '))
 
 turn = 0
-while turn >= 0 :
+while turn >= 0:
 
     # checks for remainder
-    
+
     if turn % 2 == 0:
         turn += turn
-        remove = int(input('it is your turn! pick one or two sticks to remove: '))
+        print('it is your turn! pick one through', max_Remove, 'sticks to remove: ')
+        remove = int(input('pick the amount of sticks you want to remove: '))
         sticks = sticks - remove
-        print(sticks,'remain')
+        time.sleep(1)
+        print(sticks, 'remain')
 
-    # while statements to prevent cheating from the player
-
-    while remove < 1 or remove > 2:
-        print('hey! you cant take more than 2 sticks, or less that 1 stick.')
+    while remove < 1 or remove > max_Remove:
+        time.sleep(1)
+        print('hey! you cant take more than', max_Remove, 'sticks, or less that 1 stick.')
+        time.sleep(1)
         sticks = sticks + remove
-        remove = int(input('it is your turn! pick one or two sticks to remove: '))
+        print('it is your turn! pick one through', max_Remove, 'sticks to remove: ')
+        remove = int(input('pick the amount of sticks you want to remove: '))
+        time.sleep(1)
         sticks = sticks - remove
         print(sticks, 'remain')
     while sticks < 0:
         print('you cant take more sticks than what is there!')
+        time.sleep(1)
         sticks = sticks + remove
-        remove = int(input('it is your turn! pick one or two sticks to remove: '))
+        print('it is your turn! pick one through', max_Remove, 'sticks to remove: ')
+        remove = int(input('pick the amount of sticks you want to remove: '))
+        time.sleep(1)
         sticks = sticks - remove
         print(sticks, 'remain')
 
@@ -34,28 +50,37 @@ while turn >= 0 :
 
     if sticks == 0:
         print('the player wins!')
+        time.sleep(1)
+        print('good job!')
         break
 
     # checks for remainder, shouldn't be this way, but it works, so I don't really care that much.
 
     if turn % 2 == 0:
-        remove = random.randint(1,2)
-        print ('the computer decides to take',remove, 'sticks')
+        remove = random.randint(1, max_Remove)
+        time.sleep(1)
+        print('the computer decides to take', remove, 'sticks')
+        time.sleep(1)
         sticks = sticks - remove
-        print(sticks,'remain')
+        print(sticks, 'remain')
 
     # computer does not have an actual algorithm (yet), can sometimes take 2 sticks when 1 is left.
 
     while sticks < 0:
         print('you cant take more sticks than what is there!')
         sticks = sticks + remove
-        remove = random.randint(1,2)
+        remove = random.randint(1, max_Remove)
+        time.sleep(1)
         print('the computer decides to take', remove, 'sticks')
+        time.sleep(1)
         sticks = sticks - remove
         print(sticks, 'remain')
+        time.sleep(1)
 
     # checks to see if the computer wins
 
     if sticks == 0:
         print('the computer wins!')
+        time.sleep(1)
+        print('better luck next time!')
         break
